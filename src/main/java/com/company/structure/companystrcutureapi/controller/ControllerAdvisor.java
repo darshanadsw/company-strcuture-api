fe2390db2ad7495f821646a0df884fde85f7388d
darshanadsw <darshanadsw@gmail.com>
2020-06-28 08:09:58 -0400

    changes added

--- src/main/java/com/company/structure/companystrcutureapi/controller/ControllerAdvisor.java
+++ src/main/java/com/company/structure/companystrcutureapi/controller/ControllerAdvisor.java
@@ -1,6 +1,8 @@
 package com.company.structure.companystrcutureapi.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,5 +17,12 @@ public class ControllerAdvisor {
         ModelAndView view = new ModelAndView("error404");
         view.addObject("error",e.getMessage());
         return view;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity validationError(MethodArgumentNotValidException e){
+        e.getBindingResult().getFieldErrors();
+        return ResponseEntity.badRequest().build();
     }
 }
